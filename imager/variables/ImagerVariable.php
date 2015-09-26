1a01d1addb08abf7fb69c5235aef3e8a5c90ed36
André Elvan <andre@vaersaagod.no>
2015-09-26 22:19:39 +0200

    Added srcset template variable.

--- imager/variables/ImagerVariable.php
+++ imager/variables/ImagerVariable.php
@@ -3,9 +3,26 @@ namespace Craft;
 
 class ImagerVariable
 {
+    /**
+     * @param $file
+     * @param $transform
+     * @param $configOverrides
+     * @return mixed
+     */
     public function transformImage($file, $transform, $configOverrides = null)
     {
         $image = craft()->imager->transformImage($file, $transform, $configOverrides);
         return $image;
+    }
+
+    public function srcset($images, $descriptor = 'w')
+    {
+        $r = '';
+        
+        foreach ($images as $image) {
+            $r .= $image->getUrl() . ' ' . $image->getWidth() .'w, ';
+        }
+
+        return substr($r, 0, strlen($r)-2);
     }
 }
