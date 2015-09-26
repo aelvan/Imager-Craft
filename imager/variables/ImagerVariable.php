6ad01f3d603cc710fe75ec36b56867ac4c11da96
André Elvan <andre@vaersaagod.no>
2015-09-26 22:50:34 +0200

    Added base64Pixel template variable.

--- imager/variables/ImagerVariable.php
+++ imager/variables/ImagerVariable.php
@@ -15,14 +15,34 @@ class ImagerVariable
         return $image;
     }
 
+    /**
+     * Takes an array of Imager_ImageModel (or anything else that supports getUrl() and getWidth())
+     * and returns a srcset string
+     *
+     * todo : Implement support for other descriptors
+     *
+     * @param Array $images
+     * @param string $descriptor
+     * @return string
+     */
     public function srcset($images, $descriptor = 'w')
     {
         $r = '';
-        
+
         foreach ($images as $image) {
-            $r .= $image->getUrl() . ' ' . $image->getWidth() .'w, ';
+            $r .= $image->getUrl() . ' ' . $image->getWidth() . 'w, ';
         }
 
-        return substr($r, 0, strlen($r)-2);
+        return substr($r, 0, strlen($r) - 2);
+    }
+
+    /**
+     * Returns a base64 encoded transparent pixel. Useful for adding as src on img tags for validation when using srcset.
+     * 
+     * @return string
+     */
+    public function base64Pixel()
+    {
+        return 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNgYAAAAAMAASsJTYQAAAAASUVORK5CYII=';
     }
 }
