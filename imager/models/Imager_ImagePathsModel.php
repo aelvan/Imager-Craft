92f804c9818c18740b78d2b80dfc9a5d6d7123a6
André Elvan <andre@vaersaagod.no>
2017-05-18 18:45:38 +0200

    Added fix for missing trailing slash in imagerSystemPath when transforming an external image.

--- imager/models/Imager_ImagePathsModel.php
+++ imager/models/Imager_ImagePathsModel.php
@@ -177,7 +177,7 @@ class Imager_ImagePathsModel extends BaseModel
         $runtimePath = IOHelper::getRealPath(craft()->path->getRuntimePath());
         $this->sourcePath = ImagerService::fixSlashes($runtimePath . 'imager/' . $parsedDirname . '/');
         $this->sourceUrl = $image;
-        $this->targetPath = ImagerService::fixSlashes(craft()->imager->getSetting('imagerSystemPath') . $parsedDirname . '/');
+        $this->targetPath = ImagerService::fixSlashes(craft()->imager->getSetting('imagerSystemPath') . '/' . $parsedDirname . '/');
         $this->targetUrl = craft()->imager->getSetting('imagerUrl') . ImagerService::fixSlashes($parsedDirname . '/');
         $this->sourceFilename = $this->targetFilename = str_replace(' ', '-', $pathParts['basename']);
         
