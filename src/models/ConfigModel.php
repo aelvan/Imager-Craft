ed286bc9749ce1c1c73156b1af070100bb9e4639
André Elvan <andre@vaersaagod.no>
2019-03-20 15:08:43 +0100

    Fixes regression error where settings that could have alias was not possible to override with template config overrides.

--- src/models/ConfigModel.php
+++ src/models/ConfigModel.php
@@ -24,7 +24,6 @@ class ConfigModel extends Settings
     {
         parent::__construct($config);
 
-
         // Reset model to get overrides from config file 
         foreach ($settings as $key => $value) {
             $this->$key = $value;
@@ -53,7 +52,7 @@ class ConfigModel extends Settings
         $aliasables = ['imagerSystemPath', 'imagerUrl'];
 
         foreach ($aliasables as $aliasable) {
-            $this->{$aliasable} = \Yii::getAlias($settings->{$aliasable});
+            $this->{$aliasable} = \Yii::getAlias($this->{$aliasable});
         }
     }
 
