6b5de01a051aeb4f150654fb388ee64f6456ceba
André Elvan <andre@vaersaagod.no>
2020-02-11 20:06:01 +0100

    Removed redundant url encoding for Imgix.

--- src/helpers/ImgixHelpers.php
+++ src/helpers/ImgixHelpers.php
@@ -62,22 +62,7 @@ class ImgixHelpers
         //always use forward slashes for imgix
         $path = str_replace('\\', '/', $path);
 
-        return self::getUrlEncodedPath($path);
+        return $path;
     }
     
-    /**
-     * URL encode the asset path properly
-     *
-     * @param string $path
-     *
-     * @return string
-     */
-    public static function getUrlEncodedPath($path): string
-    {
-        $entities = array('+', '%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
-        $replacements = array(' ', '!', '*', "'", '(', ')', ';', ':', '@', '&', '=', '+', '$', ',', '/', '?', '%', '#', '[', ']');
-        $path = str_replace($entities, $replacements, urlencode($path));
-        
-        return $path;
-    }    
 }
